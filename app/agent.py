0db70f3fdff81817dc6be1394d46d9a029f9cc7b
Alok Ranjan <131797730+AlokRanjanIN@users.noreply.github.com>
2025-11-06 19:28:21 +0530

    Update agent.py

--- app/agent.py
+++ app/agent.py
@@ -1,6 +1,6 @@
 from langchain_groq import ChatGroq
-from langchain.prompts import PromptTemplate
-from langchain.schema import BaseOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import BaseOutputParser
 import json
 import re
 from typing import Dict, Any
