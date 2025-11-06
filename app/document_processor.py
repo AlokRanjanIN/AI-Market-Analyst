67e3fa98cd0acbf7f75df3f62cd320bea71c0231
Alok Ranjan <131797730+AlokRanjanIN@users.noreply.github.com>
2025-11-06 21:41:43 +0530

    Update document_processor.py

--- app/document_processor.py
+++ app/document_processor.py
@@ -1,5 +1,5 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 import os
